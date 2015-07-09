db840213d15eaaefb3b18e21fed14cd23eeb43a3
Noah Buscher <me@noahbuscher.com>
2015-07-09 11:06:59 -0500

    Cleanup. Changes soon.

--- shore.js
+++ shore.js
@@ -18,9 +18,8 @@ function shore(paramobj) {
     params = paramobj;
 
     c = document.getElementById(params.view);
-    ctx = c.getContext("2d");
-    ctx.fillStyle = params.color;
-    
+	ctx = c.getContext("2d");
+	ctx.fillStyle = params.color;
     if (typeof AudioContext !== "undefined") {
         context = new AudioContext();
     } else if (typeof webkitAudioContext !== "undefined") {
@@ -28,7 +27,6 @@ function shore(paramobj) {
     } else {
         $(".hideIfNoApi").hide();
         $(".showIfNoApi").show();
-        
         return;
     }
 
@@ -37,21 +35,22 @@ function shore(paramobj) {
     for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
         window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
         window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
-       		|| window[vendors[x] + 'CancelRequestAnimationFrame'];
+                                    || window[vendors[x] + 'CancelRequestAnimationFrame'];
     }
 
     if (!window.requestAnimationFrame) {
-        window.requestAnimationFrame = function(callback, element) {
+        window.requestAnimationFrame = function (callback, element) {
             var currTime = new Date().getTime();
             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+            var id = window.setTimeout(function () { callback(currTime + timeToCall); },
+                timeToCall);
             lastTime = currTime + timeToCall;
             return id;
         };
     }
 
     if (!window.cancelAnimationFrame) {
-        window.cancelAnimationFrame = function(id) {
+        window.cancelAnimationFrame = function (id) {
             clearTimeout(id);
         };
     }
@@ -80,26 +79,21 @@ function update() {
     analyser.getByteFrequencyData(frequencyData);
 
     if (frequencyData[0] === 0) {
-    	
         // don't log
     } else {
-    	
         // the frequencyData multiplier controls the graph scale
         var gradient = ctx.createLinearGradient(0, 0, 0, c.height);
         gradient.addColorStop(0, 'white');
         gradient.addColorStop(1, params.color);
         ctx.fillStyle = gradient;
         xpos = xpos + 1;
-        
         if (xpos >= c.width - 2) {
-            var img = document.createElement('img');
-            
             xpos = c.width - 3;
+            var img = document.createElement('img');
             img.src = c.toDataURL("image/jpg");
             ctx.clearRect (0, 0, c.width, c.height);
             ctx.drawImage(img, -1, 0);
         }
-        
         ctx.fillRect(xpos, (c.height - (frequencyData[0] * params.scale)), 1, c.height);
     }
 }
